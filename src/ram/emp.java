e8a55f147d368aac6f3133527eebd28bb1d84c87
lenovo <lenovo@DESKTOP-3EO33O0>
2019-05-10 08:09:47 +0530

    tasj:102

---
+++ src/ram/emp.java
@@ -0,0 +1,20 @@
+package ram;
+
+public class emp {
+private   int  empid;
+private   String   empsal;
+public int getEmpid() {
+	return empid;
+}
+public void setEmpid(int empid) {
+	this.empid = empid;
+}
+public String getEmpsal() {
+	return empsal;
+}
+public void setEmpsal(String empsal) {
+	this.empsal = empsal;
+}
+
+
+}
